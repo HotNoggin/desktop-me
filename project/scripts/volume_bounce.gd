0470ba82f60c73de29f54e5ddfa1c7f89df9d451
Hot Noggin Studios <143744752+HotNoggin@users.noreply.github.com>
2024-11-13 15:16:18 -0600

    volume bounce

---
+++ project/scripts/volume_bounce.gd
@@ -0,0 +1,13 @@
+class_name VolumeBounce
+extends Node
+
+@export var input: VolumeInput
+@export var target: MouseLook
+@export var intensity: float = 100
+@export var offset: Vector2
+
+
+func _process(_delta: float) -> void:
+	target.offset.y = -input.current_volume * intensity
+	target.offset += offset
+	print(input.current_volume)
