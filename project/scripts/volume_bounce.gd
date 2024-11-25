daca9092ec43df197df1c2f933c0626d85b0ad5c
Hot Noggin Studios <143744752+HotNoggin@users.noreply.github.com>
2024-11-25 09:48:16 -0600

    maximum height

--- project/scripts/volume_bounce.gd
+++ project/scripts/volume_bounce.gd
@@ -3,11 +3,13 @@ extends Node
 
 @export var input: VolumeInput
 @export var target: MouseLook
-@export var intensity: float = 100
+@export var intensity: float = 1.0
+@export var maximum_height: float = 4 # Keep positive
 @export var offset: Vector2
 
 
 func _process(_delta: float) -> void:
-	target.offset.y = -input.current_volume * intensity
+	var clamped_value: float = minf(input.current_volume * intensity, maximum_height)
+	target.offset.y = -clamped_value
 	target.offset += offset
 	#print(input.current_volume)
